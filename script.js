4bec6bfb770d45073823981eaa0a74357a81c771
ISMAIL198 <mihismail90@gmail.com>
2023-02-06 13:01:10 +0600

    javascript first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// basic example of variable
+console.log(10)
+console.log('good morning')
+// number type variable 
+var bananaPrice = 12;
+console.log(bananaPrice);
+var nailsPrice = 150;
+console.log(nailsPrice);
+//  how to figure out type of a variable
+var tempareture = 30;
+console.log(typeof tempareture);
+//  boolean variable and this is the way to figure out type of variable
+var isNajesHome = true;
+console.log(typeof isNajesHome)
+//  string type of variable is this
+var bestFriend = "najes uddin";
+console.log(typeof bestFriend);
+//  I am not writing same thing duplicate just for showing the output but writing how to figure out different type of vaariable
+var myName = "ismail";
+console.log(typeof myName);
+// If I type anything number or a word or both in a multiple words we have to keep inside "doubleQoutation"
+// spacial keyword such as var should not ever be inside a quation 
+
+var myAge = 24;
+console.log(typeof myAge);
+
+var chikonTarPrice = 200;
+console.log(typeof chikonTarPrice);
+
+var myCousinName = "fahad miah"
+console.log(typeof myCousinName);
+
+var myFirstSibling = "julekha";
+console.log(typeof myFirstSibling);
+
+var isBdGood = false;
+console.log(typeof isBdGood);
+
+var isnajes1good = true;
+console.log(typeof isnajes1good);
+
+// 6th video
+var promise = "I promise I will work and learn hard untill I become a full stack web developer";
+// console.log(promise.toLowerCase())
+console.log(promise.toUpperCase());
+// this both command will help to convert this variable string either uppercase or lowercase
+var promise = "I promise I will hardwork untill I become a programmer";
+console.log(promise.indexOf("become"));
